8037dfbc900c12485606c41040b35563c9d18710
S. Suprun <SimonSuprun@gmail.com>
2021-07-01 22:16:03 +0300

    Fix port for deploy (#2)

--- src/main.py
+++ src/main.py
@@ -7,7 +7,11 @@ from app import init
 def run() -> None:
     """Run application."""
     app = init()
-    uvicorn.run(app)
+    uvicorn.run(
+        app,
+        host='0.0.0.0',
+        port=8888,
+    )
 
 
 if __name__ == "__main__":
